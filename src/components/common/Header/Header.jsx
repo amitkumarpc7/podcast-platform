05540851e116ec3801e7bc9bc712491b7d59213f
Amit Chaurasia <amitkpc11@gmail.com>
2024-02-25 15:09:27 +0530

    Signup fixed

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import './Header.css'
 import { NavLink } from 'react-router-dom';
+import {useAuthState} from "react-firebase-hooks/auth"
+import { auth } from '../../../firebase';
+
+
 
 const Header = () => {
-  return (
-    <div className='navbar'>
-      <div className="gradient">
-      </div>
-      <div className="links">
-        <NavLink to="/"> Signup </NavLink>
-        <NavLink to="/podcasts">Podcasts</NavLink>
-        <NavLink to="/create-a-podcast">Create Podcast</NavLink>
-        <NavLink to="/profile">Profile</NavLink>
+  const [user]=useAuthState(auth);
+  console.log("User is ",user)
+    return (
+      <div className='navbar'>
+        <div className="gradient">
+        </div>
+        <div className="links">
+          {!user && <NavLink to="/"> Signup </NavLink>}  
+          <NavLink to="/podcasts">Podcasts</NavLink>
+          <NavLink to="/create-a-podcast">Create Podcast</NavLink>
+          <NavLink to="/profile">Profile</NavLink>
+        </div>
+        </div>
+    );
+  
 
-      </div>
-      </div>
-  );
 };
 
 export default Header
